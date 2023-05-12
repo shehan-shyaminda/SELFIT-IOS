9b37085eddaf08e34c14db7675d34a6caa54e95e
Dinuka <shehan.shyaminda@gmail.com>
2023-05-12 11:26:06 +0530

    initial commit

---
+++ SELFIT-IOS/ViewControllers/MainViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MainViewController.swift
+//  SELFIT-IOS
+//
+//  Created by Dinuka Shehan on 2023-05-12.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .cyan
+    }
+}
